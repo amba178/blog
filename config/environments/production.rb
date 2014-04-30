657fff7ac1c342ee5b98800e9332d2a6e0359a19
salem <salemamba@gmail.com>
2014-04-30 01:52:26 -0700

    update bundle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Blog::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.force_ssl= true
 end
